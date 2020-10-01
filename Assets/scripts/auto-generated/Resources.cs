fefb9c421aa2bc85d29b7e16b4e84c90bb0c6a40
ziczach <czc85@hotmail.com>
2020-10-01 10:42:48 +0800

    Added Shader Plug-in
    Added All in One Shader Plug-In

--- Assets/scripts/auto-generated/Resources.cs
+++ Assets/scripts/auto-generated/Resources.cs
@@ -3,5 +3,64 @@ namespace k
 {
 	public static class Resources
 	{
+		public const string ALL_IN1ONE_SHADER_FUNCTIONS = "AllIn1OneShaderFunctions";
+		public const string ALL_IN1SPRITE_SHADER = "AllIn1SpriteShader";
+		public const string ALL_IN1SPRITE_SHADER_SCROLL_VIEW = "AllIn1SpriteShaderScrollView";
+		public const string ALL_IN1SPRITE_SHADER_UNSCALED_TIME = "AllIn1SpriteShaderUnscaledTime";
+		public const string AUDIO_TAG = "Prefabs/AudioTag";
+		public const string CHARACTER = "Prefabs/Character";
+		public const string CLICKABLE_SPRITE = "Prefabs/ClickableSprite";
+		public const string DRAGGABLE_SPRITE = "Prefabs/DraggableSprite";
+		public const string DRAG_TARGET_SPRITE = "Prefabs/DragTargetSprite";
+		public const string EVENT_SYSTEM = "Prefabs/EventSystem";
+		public const string FLOWCHART = "Prefabs/Flowchart";
+		public const string FUNGUS_LOGO = "Prefabs/FungusLogo";
+		public const string LOCALIZATION = "Prefabs/Localization";
+		public const string MENU_DIALOG = "Prefabs/MenuDialog";
+		public const string NARRATIVE_LOG = "Prefabs/NarrativeLog";
+		public const string NARRATIVE_LOG_ENTRY_DISPLAY = "Prefabs/NarrativeLogEntryDisplay";
+		public const string PARALLAX_SPRITE = "Prefabs/ParallaxSprite";
+		public const string SAVE_DATA = "Prefabs/SaveData";
+		public const string SAVE_MENU = "Prefabs/SaveMenu";
+		public const string SAY_DIALOG = "Prefabs/SayDialog";
+		public const string STAGE = "Prefabs/Stage";
+		public const string STAGE_POSITION = "Prefabs/StagePosition";
+		public const string TAG = "Prefabs/Tag";
+		public const string VIEW = "Prefabs/View";
+		public const string BACKGROUND = "Sprites/Background";
+		public const string HAND_POINTER = "Sprites/HandPointer";
+		public const string MOUSE_POINTER = "Sprites/MousePointer";
+		public const string FUNGUS = "Lua/fungus";
+		public const string INSPECT = "Lua/inspect";
+		public const string INFO_TEXT = "Prefabs/InfoText";
+		public const string LUA = "Prefabs/Lua";
+		public const string LUA_BINDINGS = "Prefabs/LuaBindings";
+		public const string LUA_ENVIRONMENT = "Prefabs/LuaEnvironment";
+		public const string LUA_SCRIPT = "Prefabs/LuaScript";
+		public const string LUA_STORE = "Prefabs/LuaStore";
+		public const string FUNGUS_TYPES = "Types/FungusTypes";
+		public const string UNITY_TYPES = "Types/UnityTypes";
+		public const string LINE_BREAKING_FOLLOWING_CHARACTERS = "LineBreaking Following Characters";
+		public const string LINE_BREAKING_LEADING_CHARACTERS = "LineBreaking Leading Characters";
+		public const string TMP_SETTINGS = "TMP Settings";
+		public const string LIBERATION_SANS_SDF___DROP_SHADOW = "Fonts & Materials/LiberationSans SDF - Drop Shadow";
+		public const string LIBERATION_SANS_SDF___OUTLINE = "Fonts & Materials/LiberationSans SDF - Outline";
+		public const string LIBERATION_SANS_SDF = "Fonts & Materials/LiberationSans SDF";
+		public const string TMPRO = "Shaders/TMPro";
+		public const string TMPRO_PROPERTIES = "Shaders/TMPro_Properties";
+		public const string TMPRO_SURFACE = "Shaders/TMPro_Surface";
+		public const string TMP_BITMAP_CUSTOM_ATLAS = "Shaders/TMP_Bitmap-Custom-Atlas";
+		public const string TMP_BITMAP_MOBILE = "Shaders/TMP_Bitmap-Mobile";
+		public const string TMP_BITMAP = "Shaders/TMP_Bitmap";
+		public const string TMP_SDF_OVERLAY = "Shaders/TMP_SDF Overlay";
+		public const string TMP_SDF_MOBILE_MASKING = "Shaders/TMP_SDF-Mobile Masking";
+		public const string TMP_SDF_MOBILE_OVERLAY = "Shaders/TMP_SDF-Mobile Overlay";
+		public const string TMP_SDF_MOBILE = "Shaders/TMP_SDF-Mobile";
+		public const string TMP_SDF_SURFACE_MOBILE = "Shaders/TMP_SDF-Surface-Mobile";
+		public const string TMP_SDF_SURFACE = "Shaders/TMP_SDF-Surface";
+		public const string TMP_SDF = "Shaders/TMP_SDF";
+		public const string TMP_SPRITE = "Shaders/TMP_Sprite";
+		public const string EMOJI_ONE = "Sprite Assets/EmojiOne";
+		public const string DEFAULT_STYLE_SHEET = "Style Sheets/Default Style Sheet";
 	}
 }
