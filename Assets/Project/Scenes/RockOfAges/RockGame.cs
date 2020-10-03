2cdacb5d19421868505d905eedcd702242585954
Justin Wong <jusw85@hotmail.com>
2020-10-03 15:46:50 +0800

    Add bgm

---
+++ Assets/Project/Scenes/RockOfAges/RockGame.cs
@@ -0,0 +1,15 @@
+﻿using Jusw85.Common;
+using Prime31;
+using UnityEngine;
+
+public class RockGame : MonoBehaviour
+{
+    [SerializeField] private AudioClip bgm;
+    private SoundKit soundKit;
+
+    private void Start()
+    {
+        soundKit = Toolbox.Instance.TryGet<SoundKit>();
+        soundKit.playBackgroundMusic(bgm, 1.0f);
+    }
+}
