10caa1f94bfeb09fa32ed75ec139d4a646334f1b
ziczach <czc85@hotmail.com>
2020-10-03 14:52:15 +0800

    level 2 tiled map added

---
+++ Assets/Project/Tiled/Ground_896x448.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Ground_896x448" tilewidth="896" tileheight="448" tilecount="1" columns="1">
+ <image source="Spritesheet/Ground_896x448.png" width="896" height="448"/>
+</tileset>
