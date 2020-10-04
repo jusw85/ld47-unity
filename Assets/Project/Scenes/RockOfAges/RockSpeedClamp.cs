f709f66e656144c55cfb3605198af4852155d57f
Justin Wong <jusw85@hotmail.com>
2020-10-04 23:21:58 +0800

    Add rock speed clamp

---
+++ Assets/Project/Scenes/RockOfAges/RockSpeedClamp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Jusw85.Common;
+using Prime31;
+using UnityEngine;
+
+public class RockSpeedClamp : MonoBehaviour
+{
+    [SerializeField] private float maxHorizontalVelocity = 2f;
+    [SerializeField, HideInInspector] private Rigidbody2D rb2d;
+    
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 vel = rb2d.velocity;
+        vel.x = Mathf.Clamp(vel.x, -maxHorizontalVelocity, maxHorizontalVelocity);
+        rb2d.velocity = vel;
+    }
+}
