c913cd2cd98ff5abb2e3f63bf1f151f6ff60247b
Justin Wong <jusw85@hotmail.com>
2020-10-05 16:40:29 +0800

    Add music trigger

---
+++ Assets/Project/Scenes/RockOfAges/NextMusicTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Jusw85.Common;
+using Prime31;
+using UnityEngine;
+
+public class NextMusicTrigger : MonoBehaviour
+{
+    private SoundKit sk;
+    [SerializeField] private AudioClip clip;
+    private void Start()
+    {
+        sk = Toolbox.Instance.TryGet<SoundKit>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        sk.playBackgroundMusic(clip, 1.0f);
+        gameObject.SetActive(false);
+    }
+}
