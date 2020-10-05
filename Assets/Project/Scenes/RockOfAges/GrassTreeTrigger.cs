3df5d6c2d313e39dbd44aee750e244dd3a92c406
Justin Wong <jusw85@hotmail.com>
2020-10-05 17:03:53 +0800

    Update sissyphus animation

---
+++ Assets/Project/Scenes/RockOfAges/GrassTreeTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Jusw85.Common;
+using k;
+using Prime31;
+using UnityEngine;
+
+public class GrassTreeTrigger : MonoBehaviour
+{
+    private GameObject treeObj;
+    private GameObject grassObj;
+    private void Start()
+    {
+        treeObj = GameObject.FindGameObjectWithTag(Tags.TREES);
+        grassObj = GameObject.FindGameObjectWithTag(Tags.GRASS);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Animator[] treeAnimators = treeObj.GetComponentsInChildren<Animator>();
+        
+    }
+}
